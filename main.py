b5f81115eec1ada15cbecd9273b94220f0db8ac2
nabhpatodi10 <nabhpatodi1005@gmail.com>
2024-07-17 15:29:42 +0530

    storevector update

--- main.py
+++ main.py
@@ -1,17 +1,45 @@
 from fastapi import FastAPI
 import uvicorn
+from pydantic import BaseModel
+from langchain.vectorstores import MongoDBAtlasVectorSearch
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import TextLoader
+import os
 from db.db import connect_to_mongo
 
 app = FastAPI()
-connect_to_mongo()
+collection = connect_to_mongo()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+class content(BaseModel):
+    text: str
+
 @app.post("/storevector")
-def storevector():
-    pass
+def storevector(text: content):
+    with open("db\Coding Ninjas.txt", "a") as file:
+        file.write(text.text)
+    file.close()
+    file = open("db\Coding Ninjas.txt")
+    cont = file.read()
+
+    loader = TextLoader("db\Coding Ninjas.txt")
+    docs = loader.load()
+    text_spiliter = RecursiveCharacterTextSplitter(chunk_size=600, chunk_overlap=20)
+    splits = text_spiliter.split_documents(docs)
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001",google_api_key=os.getenv('GEMINI_API_KEY'))
+    store = [] 
+    embedding = []
+    for split in splits:
+        store.append(split.page_content)
+    
+    docsearch = MongoDBAtlasVectorSearch.from_documents(splits, embeddings, collection=collection, index_name="embeddings")
+    
+    #return docsearch
+    return "Embeddings saved to MongoDB vector base"
 
 @app.post("/queryresult")
 def queryResult():
